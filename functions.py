548f7d86cc5c21eb71e7c95739bb3cdecd8bc7f0
James Rezendes <James@Johns-MacBook-Pro.local>
2017-09-07 21:19:25 -0700

    added recursion

--- functions.py
+++ functions.py
@@ -2,6 +2,7 @@
 import sys
 userInput = int(sys.argv[1])
 
+
 def fibonacci_iterative(num):
     #New to python, so I don't know how to check if a variable was not given a value. I could do that in javascript though
     currentNum = 1
@@ -16,4 +17,13 @@ fibonacci_iterative(userInput)
 
 
 #Recursion:
-#After 30 minutes I realized I was going about it wrongly, I'm not sure how to do it
+def factorial_recursive(num):
+    if(num < 1):
+        return 1
+
+    newNum = num * factorial_recursive(num-1)
+    return newNum
+    print(newNum)
+
+
+print(factorial_recursive(8))
