20acd2f937bd46309a9e25804957f865e0280636
James Rezendes <James@Johns-MacBook-Pro.local>
2017-09-07 17:34:07 -0700

    added functions.py

---
+++ functions.py
@@ -0,0 +1,16 @@
+#Functions
+import sys
+userInput = int(sys.argv[1])
+
+def fibonacci_iterative(num):
+    #New to python, so I don't know how to check if a variable was not given a value. I could do that in javascript though
+    currentNum = 1
+    prevNum = 0
+    for number in range(0, num):
+        sum = currentNum + prevNum
+        print(sum)
+        prevNum = currentNum
+        currentNum = sum
+
+
+fibonacci_iterative(userInput)
